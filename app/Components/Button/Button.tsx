49c7ccddea722807c5a20e08c718958b7280ede0
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-27 14:52:49 +0600

    add navbar

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -2,8 +2,8 @@ import React, { ButtonHTMLAttributes, FC } from "react";
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Input: FC<IProps> = ({ children, ...props }) => (
+const Button: FC<IProps> = ({ children, ...props }) => (
   <button {...props}>{children}</button>
 );
 
-export default Input;
+export default Button;
--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,12 +1,78 @@
 import Image from "next/image";
 import Link from "next/link";
-import { menuData } from "../../data";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Button from "../Button/Button";
 
 const Navbar = () => {
+  const router = useRouter();
+  const nav_links = `text-gray-700 text-sm font-semibold transition-all duration-300 hover:text-red-500`;
+  const nav_active = `text-red-500`;
+
+  const menuData = [
+    {
+      id: 1,
+      url: "/",
+      name: "Home",
+      className:
+        router.pathname == "/" ? `${nav_links} ${nav_active}` : `${nav_links}`,
+    },
+    {
+      id: 2,
+      url: "/music",
+      name: "Music",
+      className:
+        router.pathname == "/music"
+          ? `${nav_links} ${nav_active}`
+          : `${nav_links}`,
+    },
+    {
+      id: 13,
+      url: "/premium",
+      name: "Premium",
+      className:
+        router.pathname == "/premium"
+          ? `${nav_links} ${nav_active}`
+          : `${nav_links}`,
+    },
+    {
+      id: 4,
+      url: "/contact",
+      name: "Contact",
+      className:
+        router.pathname == "/contact"
+          ? `${nav_links} ${nav_active}`
+          : `${nav_links}`,
+    },
+  ];
+
+  const afterLogin = [
+    {
+      id: 1,
+      url: "/profile",
+      name: "Profile",
+      className:
+        router.pathname == "/profile"
+          ? `${nav_links} ${nav_active}`
+          : `${nav_links}`,
+    },
+    {
+      id: 2,
+      url: "/favorites",
+      name: "Favorites",
+      className:
+        router.pathname == "/favorites"
+          ? `${nav_links} ${nav_active}`
+          : `${nav_links}`,
+    },
+  ];
+
+  const [menu, setMenu] = useState(false);
+
   return (
     <>
-      <header>
+      <header className="p-2 shadow-md">
         <div className="container mx-auto">
           <nav className="flex justify-between items-center">
             {/* Logo */}
@@ -15,16 +81,20 @@ const Navbar = () => {
             </Link>
             {/* Navigation Items */}
             <ul className="flex gap-4 items-center">
-              {menuData.map(({ id, url, name }) => (
+              {menuData.map(({ id, url, name, className }) => (
                 <li key={id}>
                   <Link href={url}>
-                    <a>{name}</a>
+                    <a className={className}>{name}</a>
                   </Link>
                 </li>
               ))}
             </ul>
             {/* User Information*/}
-            <div className="user">
+            <div
+              className="user cursor-pointer relative"
+              onMouseEnter={() => setMenu(!menu)}
+              onMouseLeave={() => setMenu(!menu)}
+            >
               <Image
                 src="https://randomuser.me/api/portraits/men/88.jpg"
                 width={50}
@@ -32,6 +102,25 @@ const Navbar = () => {
                 alt="Razu Islam"
                 className="object-cover"
               />
+              {menu && (
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  className="absolute top-[50px] bg-gray-200 p-4 rounded-sm leading-7"
+                >
+                  {afterLogin.map(({ id, url, name, className }) => (
+                    <li key={id}>
+                      <Link href={url}>
+                        <a className={`${className} text-gray-500`}>{name}</a>
+                      </Link>
+                    </li>
+                  ))}
+                  <Button className="px-5 py-1 text-white transition-all duration-300 bg-red-400 mt-2 hover:bg-green-400">
+                    Logout
+                  </Button>
+                </motion.div>
+              )}
             </div>
           </nav>
         </div>
