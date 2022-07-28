d93efbc417095e04fd6ce9938455763745e58a04
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-28 13:45:08 +0600

    added delete button in every components

--- app/Components/ArtistCard/index.tsx
+++ app/Components/ArtistCard/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React, { FC, ReactNode } from 'react'
 import { IArtis } from '../../utils'
+import { motion } from 'framer-motion'
+import { MdDelete } from 'react-icons/md'
 
 
 interface IProps{
@@ -11,7 +13,7 @@ interface IProps{
 const ArtistCard:FC<IProps> = ({ artist, children }) => {
   return (
     <>
-      <div className="shadow-lg rounded-lg p-8 w-[100%] sm:w-[50%] md:w-[30%] lg:w-[15%] text-center">
+      <div className="shadow-lg rounded-lg p-8 w-[100%] sm:w-[50%] md:w-[30%] lg:w-[15%] text-center relative">
       <Image
         src={artist.imageUrl}
         width={120}
@@ -21,6 +23,12 @@ const ArtistCard:FC<IProps> = ({ artist, children }) => {
       />
       <span className="text block">{artist.name}</span>
       {children}
+      <motion.div
+        whileTap={{ scale: 0.75 }}
+        className="absolute cursor-pointer left-4 bottom-4 w-8 h-8 rounded-md flex items-center justify-center bg-gray-200"
+      >
+        <MdDelete className="text-xl text-red-400 hover:text-red-500" />
+      </motion.div>
     </div>
     </>
   )
--- app/Components/Card/Card.tsx
+++ app/Components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React, { FC } from "react";
 import { IMusic } from "../../utils";
+import { motion } from "framer-motion";
+import { MdDelete } from "react-icons/md";
 
 interface IProps {
   music: IMusic;
@@ -8,7 +10,7 @@ interface IProps {
 
 const Card: FC<IProps> = ({ music }) => {
   return (
-    <div className="shadow-lg rounded-lg p-8 w-[100%] sm:w-[50%] md:w-[30%] lg:w-[15%] text-center">
+    <div className="shadow-lg rounded-lg p-4 w-[100%] sm:w-[50%] md:w-[30%] lg:w-[15%] text-center relative">
       <Image
         src={music.imageUrl}
         width={120}
@@ -18,6 +20,12 @@ const Card: FC<IProps> = ({ music }) => {
       />
       <h3 className="text-gray-800 font-semibold text-lg">{music.name}</h3>
       <span className="text">{music.artist.name}</span>
+      <motion.div
+        whileTap={{ scale: 0.75 }}
+        className="absolute cursor-pointer left-4 bottom-4 w-8 h-8 rounded-md flex items-center justify-center bg-gray-200"
+      >
+        <MdDelete className="text-xl text-red-400 hover:text-red-500" />
+      </motion.div>
     </div>
   );
 };
