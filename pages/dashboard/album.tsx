67908d8ee2369a4fd7eff1692b1f8b9ba3f9eb1b
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-28 13:40:26 +0600

    added album features

--- pages/dashboard/album.tsx
+++ pages/dashboard/album.tsx
@@ -1,47 +1,58 @@
-import React from 'react'
-import DashboardNavbar from '../../app/Components/DashboardNavbar/DashboardNavbar'
-import { motion } from 'framer-motion'
-import ArtistCard from '../../app/Components/ArtistCard'
-
-
-const data = {
-  _id:"1",
-  name: 'Artist',
-  imageUrl: "https://randomuser.me/api/portraits/men/88.jpg"
-}
+import React, { useEffect, useState } from "react";
+import DashboardNavbar from "../../app/Components/DashboardNavbar/DashboardNavbar";
+import { motion } from "framer-motion";
+import ArtistCard from "../../app/Components/ArtistCard";
+import { getAlbums } from "../../app/api";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { IAlbum } from "../../app/utils";
+import Loader from "../../app/Components/Loader/Loader";
 
 const Album = () => {
+  const [album, setAlbum] = useState<IAlbum[]>();
+  const [loading, setLoading] = useState(false);
+  const { token } = useSelector((state: RootState) => state.auth);
+
+  const getAlbum = async () => {
+    const data = await getAlbums(token);
+    setLoading(false);
+    setAlbum(data);
+  };
+
+  useEffect(() => {
+    token && getAlbum();
+  }, [token]);
+
   return (
     <>
-     <section className="sec_p">
-        <div className="container mx-auto">
-          <DashboardNavbar />
-          {/* Render Music List */}
-          <div className="mt-10 w-full flex flex-col items-start justify-start p-4 border border-gray-300 rounded-md gap-3">
-            <p className="text-xl font-bold">
-              <span className="text-sm font-semibold text-left mr-2">
-                Count :
-              </span>
-              2
-            </p>
-            <motion.div
-            className="flex justify-between items-center mt-[3em] flex-wrap w-full"
-            initial={{ opacity: 0, translateX: -50 }}
-            animate={{ opacity: 1, translateX: 0 }}
-            transition={{ duration: 0.3, delay: 0.1 }}
-          >
-            <ArtistCard artist={data} />
-            <ArtistCard artist={data} />
-            <ArtistCard artist={data} />
-            <ArtistCard artist={data} />
-            <ArtistCard artist={data} />
-          </motion.div>
-
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="sec_p">
+          <div className="container mx-auto">
+            <DashboardNavbar />
+            {/* Render Music List */}
+            <div className="mt-10 w-full flex flex-col items-start justify-start p-4 border border-gray-300 rounded-md gap-3">
+              <p className="text-xl font-bold">
+                <span className="text-sm font-semibold text-left mr-2">
+                  Count :
+                </span>
+                2
+              </p>
+              <motion.div
+                className="flex justify-between items-center mt-[3em] flex-wrap w-full"
+                initial={{ opacity: 0, translateX: -50 }}
+                animate={{ opacity: 1, translateX: 0 }}
+                transition={{ duration: 0.3, delay: 0.1 }}
+              >
+                {album && album.map((album) => <ArtistCard artist={album} />)}
+              </motion.div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Album
+export default Album;
--- pages/dashboard/artist.tsx
+++ pages/dashboard/artist.tsx
@@ -7,6 +7,7 @@ import { IArtis } from "../../app/utils";
 import { getArtists } from "../../app/api";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
+import Loader from "../../app/Components/Loader/Loader";
 
 const Artist = () => {
   const [artist, setArtist] = useState<IArtis[]>();
@@ -25,49 +26,54 @@ const Artist = () => {
 
   return (
     <>
-      <section className="sec_p">
-        <div className="container mx-auto">
-          <DashboardNavbar />
-          {/* Render Music List */}
-          <div className="mt-10 w-full flex flex-col items-start justify-start p-4 border border-gray-300 rounded-md gap-3">
-            <p className="text-xl font-bold">
-              <span className="text-sm font-semibold text-left mr-2">
-                Count :
-              </span>
-            </p>
-            <motion.div
-              className="flex justify-between items-center mt-[3em] flex-wrap w-full"
-              initial={{ opacity: 0, translateX: -50 }}
-              animate={{ opacity: 1, translateX: 0 }}
-              transition={{ duration: 0.3, delay: 0.1 }}
-            >
-              {artist && artist.map((artist) => (
-                <ArtistCard artist={artist}>
-                <div className="flex justify-center items-center gap-2">
-                  <a
-                    href={artist.linkedinLink}
-                    rel="noreferrer"
-                    target="_blank"
-                    className="text-blue-500"
-                  >
-                    <FaLinkedinIn />
-                  </a>
-                  <a
-                    href={artist.instagramLink}
-                    rel="noreferrer"
-                    target="_blank"
-                    className="text-yellow-500"
-                  >
-                    <FaInstagram />
-                  </a>
-                </div>
-              </ArtistCard>
-              ))}
-              
-            </motion.div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="sec_p">
+          <div className="container mx-auto">
+            <DashboardNavbar />
+            {/* Render Music List */}
+            <div className="mt-10 w-full flex flex-col items-start justify-start p-4 border border-gray-300 rounded-md gap-3">
+              <p className="text-xl font-bold">
+                <span className="text-sm font-semibold text-left mr-2">
+                  Count :
+                </span>
+                {artist && artist?.length}
+              </p>
+              <motion.div
+                className="flex justify-between items-center mt-[3em] flex-wrap w-full"
+                initial={{ opacity: 0, translateX: -50 }}
+                animate={{ opacity: 1, translateX: 0 }}
+                transition={{ duration: 0.3, delay: 0.1 }}
+              >
+                {artist &&
+                  artist.map((artist) => (
+                    <ArtistCard artist={artist}>
+                      <div className="flex justify-center items-center gap-2">
+                        <a
+                          href={artist.linkedinLink}
+                          rel="noreferrer"
+                          target="_blank"
+                          className="text-blue-500"
+                        >
+                          <FaLinkedinIn />
+                        </a>
+                        <a
+                          href={artist.instagramLink}
+                          rel="noreferrer"
+                          target="_blank"
+                          className="text-yellow-500"
+                        >
+                          <FaInstagram />
+                        </a>
+                      </div>
+                    </ArtistCard>
+                  ))}
+              </motion.div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 };
