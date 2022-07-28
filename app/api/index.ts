ffcc727b9739afecd71e8429957a8d9eca814211
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-28 11:27:10 +0600

    fetch getAllSong and update ui

---
+++ app/api/index.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { apiEndPoint } from "../utils";
+
+export const getUsers = async (token: string) => {
+  try {
+    const { data } = await axios.get(`${apiEndPoint}/admin/users`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data?.user;
+  } catch (error: any) {
+    return error.message;
+  }
+};
+
+export const getArtists = async (token: string) => {
+  try {
+    const { data } = await axios.get(`${apiEndPoint}/artist/getAllArtist`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data?.artist;
+  } catch (error: any) {
+    return error.message;
+  }
+};
+
+export const getSongs = async (token: string) => {
+  try {
+    const { data } = await axios.get(`${apiEndPoint}/song/getAllSong`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data?.song;
+  } catch (error: any) {
+    return error.message;
+  }
+};
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,46 +1,84 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { FaUserFriends } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { getArtists, getSongs, getUsers } from "../app/api";
 import DashboardCard from "../app/Components/DashboardCard/DashboardCard";
 import DashboardNavbar from "../app/Components/DashboardNavbar/DashboardNavbar";
+import Loader from "../app/Components/Loader/Loader";
+import { RootState } from "../app/store";
+import { IArtis, IMusic, IUser } from "../app/utils";
 
 const Dashboard = () => {
+  const [loading, setLoading] = useState(true);
+  const { token } = useSelector((state: RootState) => state.auth);
+  const [users, setUsers] = useState<IUser[]>();
+  const [artist, setArtist] = useState<IArtis[]>();
+  const [songs, setSongs] = useState<IMusic[]>();
 
+  const getUser = async () => {
+    const data = await getUsers(token);
+    setLoading(false)
+    setUsers(data)
+  };
 
+  const getArtist = async () => {
+    const data = await getArtists(token);
+    setLoading(false)
+    setArtist(data)
+  };
 
-  
+  const getSong = async () => {
+    const data = await getSongs(token);
+    setLoading(false)
+    setSongs(data)
+  };
+
+  useEffect(() => {
+    token && getUser();
+    token && getArtist();
+    token && getSong();
+  }, [token]);
+
   return (
     <>
-      <section className="sec_p">
+      {loading ? <Loader /> : (
+        <section className="sec_p">
         <div className="container mx-auto">
           <DashboardNavbar />
           <div className="mt-8 flex items-center gap-10 flex-wrap">
-            <DashboardCard
+            {users && artist && songs && (
+              <>
+              <DashboardCard
               icon={<FaUserFriends />}
               name="Users"
-              length={2}
+              length={users && users?.length}
               color="bg-red-400"
             />
             <DashboardCard
               icon={<FaUserFriends />}
               name="Artist"
-              length={2}
+              length={artist && artist?.length}
               color="bg-green-400"
             />
             <DashboardCard
               icon={<FaUserFriends />}
               name="Songs"
-              length={2}
+              length={songs && songs?.length}
               color="bg-yellow-400"
             />
             <DashboardCard
               icon={<FaUserFriends />}
               name="Album"
-              length={2}
+              length={users && users?.length}
               color="bg-emerald-400"
             />
+              </>
+            )}
           </div>
         </div>
       </section>
+      )}
     </>
   );
 };
